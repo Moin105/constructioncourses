d88669f8f9e8ffc6814b3c8059684d3e4f0f66f7
moin105 <moinlatif105@gmail.com>
2024-02-29 03:46:46 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import styles from "./style";
+
+export default function Home() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+    </div>
+  );
+}
